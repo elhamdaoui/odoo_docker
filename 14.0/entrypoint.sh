5165c4400d019ddc09a1f280f9924fc98dd7d78f
Abdelmajid Elhamdaoui <elhamdaoui>
2021-12-15 21:34:36 +0100

    FIX

--- 14.0/entrypoint.sh
+++ 14.0/entrypoint.sh
@@ -10,8 +10,8 @@ fi
 # and pass them as arguments to the odoo process if not present in the config file
 : ${HOST:=${DB_PORT_5432_TCP_ADDR:='db'}}
 : ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
+: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='odoo14'}}}
+: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='odoo14'}}}
 
 DB_ARGS=()
 function check_config() {
