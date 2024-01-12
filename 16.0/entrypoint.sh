948105555fcf271ac3f04ab43409a3ce3ea712b0
Anpartner <anpartner@pcs-MacBook-Pro.com>
2024-01-13 00:11:10 +0100

    [ADD] 16.0 arm64 for mac os

--- 16.0/entrypoint.sh
+++ 16.0/entrypoint.sh
@@ -32,15 +32,15 @@ case "$1" in
     -- | odoo)
         shift
         if [[ "$1" == "scaffold" ]] ; then
-            exec odoo "$@"
+            exec /opt/odoo/odoo-server/odoo-bin -c /etc/odoo/odoo.conf "$@"
         else
             wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-            exec odoo "$@" "${DB_ARGS[@]}"
+            exec /opt/odoo/odoo-server/odoo-bin -c /etc/odoo/odoo.conf "$@" "${DB_ARGS[@]}"
         fi
         ;;
     -*)
         wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-        exec odoo "$@" "${DB_ARGS[@]}"
+        exec /opt/odoo/odoo-server/odoo-bin -c /etc/odoo/odoo.conf "$@" "${DB_ARGS[@]}"
         ;;
     *)
         exec "$@"
